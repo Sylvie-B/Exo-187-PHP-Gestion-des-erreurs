c4ee7a9061d8d851dd36d98e1a8235b2a489f8b2
Sylvie-B <unsolo@hotmail.fr>
2021-02-12 15:43:28 +0100

    erreur get message

--- index.php
+++ index.php
@@ -22,9 +22,11 @@ function divide(int $number, int $by) {
 // TODO Compléter le code en fournissent un OU deux zéro en paramètre de la fonction divide !
 try {
     // Corrigez moi ( si je me trompe ! ).
-    $result = divide();
+    $result = divide(2,0);
     // Afficher la variable $result.
+    echo $result;
 }
-catch(//TODO Complétez moi !) {
+catch(Exception $e) {
     // TODO afficher le message contenu dans $e.
+    echo $e->getMessage();
 }
